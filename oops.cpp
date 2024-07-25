ea902fd407eeeecd63b0b891e4f272f8d79b53f3
harisiqbal8 <harisiqbaldec@gmail.com>
2024-07-25 19:17:56 +0500

    Project Upload

---
+++ oops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ABC
+{
+public:
+    ABC()
+    {
+        cout << "Constructor\n";
+    }
+    ~ABC()
+    {
+        cout << "Destructor\n";
+    }
+};
+
+int main()
+{
+    if (true)
+    {
+        static ABC obj;
+    }
+
+    cout << "End of main fnx\n";
+    return 0;
+}
